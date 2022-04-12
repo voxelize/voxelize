6fb34e66eecf51881ed32c0ec9cd52722711b5c1
Ian Huang <21ianh1@students.tas.tw>
2022-04-12 16:38:29 -0500

    feat: registry comments

--- server/world/registry.rs
+++ server/world/registry.rs
@@ -26,18 +26,24 @@ pub struct Registry {
     /// A map of the UV's to the block faces on the texture atlas.
     pub ranges: HashMap<String, UV>,
 
+    /// Block records, name -> Block.
     blocks_by_name: HashMap<String, Block>,
 
+    /// Block records, id -> Block.
     blocks_by_id: HashMap<u32, Block>,
 
+    /// List of textures that this registry has. Textures are then applied onto block sides.
     textures: HashSet<String>,
 
+    /// Map of ID -> name.
     name_map: HashMap<u32, String>,
 
+    /// Map of name -> ID.
     type_map: HashMap<String, u32>,
 }
 
 impl Registry {
+    /// Create a registry instance. By default, the "Air" block is registered at ID of 0.
     pub fn new() -> Self {
         let air = Block::new("Air")
             .is_solid(false)
@@ -46,9 +52,13 @@ impl Registry {
             .is_transparent(true)
             .build();
 
-        Self::default()
+        let mut instance = Self::default();
+        instance.register_block(air);
+
+        instance
     }
 
+    /// Generate the UV coordinates of the blocks. Call this before the server starts!
     pub fn generate(&mut self) {
         let count_per_side = self.per_side();
 
@@ -83,24 +93,28 @@ impl Registry {
         }
     }
 
+    /// Register a block into this world. The block ID is assigned to the length of the blocks registered.
     pub fn register_block(&mut self, mut block: Block) -> Block {
         block.id = self.blocks_by_name.len() as u32;
         self.record_block(&block);
         block
     }
 
+    /// Get a block reference by block name.
     pub fn get_block_by_name(&self, name: &str) -> &Block {
         self.blocks_by_name
             .get(name)
             .unwrap_or_else(|| panic!("Block name not found: {name}",))
     }
 
+    /// Get a block reference by block ID.
     pub fn get_block_by_id(&self, id: u32) -> &Block {
         self.blocks_by_id
             .get(&id)
             .unwrap_or_else(|| panic!("Block id not found: {id}"))
     }
 
+    /// Get a block id by block name.
     pub fn get_id_by_name(&self, name: &str) -> u32 {
         *self
             .type_map
@@ -108,87 +122,87 @@ impl Registry {
             .unwrap_or_else(|| panic!("Block name not found: {name}"))
     }
 
-    /// Get block transparency by id
+    /// Get block transparency by id.
     pub fn get_transparency_by_id(&self, id: u32) -> bool {
         self.get_block_by_id(id).is_transparent
     }
 
-    /// Get block transparency by name
+    /// Get block transparency by name.
     pub fn get_transparency_by_name(&self, name: &str) -> bool {
         self.get_block_by_name(name).is_transparent
     }
 
-    /// Get block fluidity by id
+    /// Get block fluidity by id.
     pub fn get_fluiditiy_by_id(&self, id: u32) -> bool {
         self.get_block_by_id(id).is_fluid
     }
 
-    /// Get block fluidity by name
+    /// Get block fluidity by name.
     pub fn get_fluiditiy_by_name(&self, name: &str) -> bool {
         self.get_block_by_name(name).is_fluid
     }
 
-    /// Get block solidity by id
+    /// Get block solidity by id.
     pub fn get_solidity_by_id(&self, id: u32) -> bool {
         self.get_block_by_id(id).is_solid
     }
 
-    /// Get block solidity by name
+    /// Get block solidity by name.
     pub fn get_solidity_by_name(&self, name: &str) -> bool {
         self.get_block_by_name(name).is_solid
     }
 
-    /// Get block emptiness by id
+    /// Get block emptiness by id.
     pub fn get_emptiness_by_id(&self, id: u32) -> bool {
         self.get_block_by_id(id).is_empty
     }
 
-    /// Get block emptiness by name
+    /// Get block emptiness by name.
     pub fn get_emptiness_by_name(&self, name: &str) -> bool {
         self.get_block_by_name(name).is_empty
     }
 
-    /// Get block faces by id
+    /// Get block faces by id.
     pub fn get_faces_by_id(&self, id: u32) -> &Vec<BlockFaces> {
         &self.get_block_by_id(id).faces
     }
 
-    /// Get block faces by name
+    /// Get block faces by name.
     pub fn get_faces_by_name(&self, name: &str) -> &Vec<BlockFaces> {
         &self.get_block_by_name(name).faces
     }
 
-    /// Get block UV by id
+    /// Get block UV by id.
     pub fn get_uv_by_id(&self, id: u32) -> HashMap<String, &UV> {
         self.get_uv_map(self.get_block_by_id(id))
     }
 
-    /// Get block UV by name
+    /// Get block UV by name.
     pub fn get_uv_by_name(&self, name: &str) -> HashMap<String, &UV> {
         self.get_uv_map(self.get_block_by_name(name))
     }
 
-    /// Check if block is air by id
+    /// Check if block is air by id.
     pub fn is_air(&self, id: u32) -> bool {
         self.get_block_by_id(id).name == "Air"
     }
 
-    /// Check if block is fluid by id
+    /// Check if block is fluid by id.
     pub fn is_fluid(&self, id: u32) -> bool {
         self.get_block_by_id(id).is_fluid
     }
 
-    /// Check if block is a plant by id
+    /// Check if block is a plant by id.
     pub fn is_plant(&self, id: u32) -> bool {
         self.get_block_by_id(id).is_plant
     }
 
-    /// Check if block is plantable by id
+    /// Check if block is plantable by id.
     pub fn is_plantable(&self, id: u32, above: u32) -> bool {
         self.get_block_by_id(id).is_plantable && self.get_block_by_id(above).is_empty
     }
 
-    /// Get UV map by block
+    /// Get UV map by block.
     pub fn get_uv_map(&self, block: &Block) -> HashMap<String, &UV> {
         let mut uv_map = HashMap::new();
 
@@ -205,7 +219,7 @@ impl Registry {
         uv_map
     }
 
-    /// Get type map of all blocks
+    /// Get type map of all blocks.
     pub fn get_type_map(&self, blocks: Vec<&str>) -> HashMap<String, u32> {
         let mut type_map = HashMap::new();
 
@@ -221,7 +235,7 @@ impl Registry {
         type_map
     }
 
-    /// Get solids that can be treated as empty's
+    /// Get solids that can be treated as empty's.
     pub fn get_passable_solids(&self) -> Vec<u32> {
         self.blocks_by_id
             .iter()
@@ -230,47 +244,25 @@ impl Registry {
             .collect()
     }
 
-    /// Check if registry contains type
+    /// Check if registry contains type.
     pub fn has_type(&self, id: u32) -> bool {
         self.blocks_by_id.contains_key(&id)
     }
 
-    pub fn per_side(&self) -> usize {
-        let mut i = 1.0;
-        let sqrt = (self.textures.len() as f32).sqrt().ceil();
-        while i < sqrt {
-            i = i * 2.0;
-        }
-        i as usize
-    }
-
-    pub fn record_block(&mut self, block: &Block) {
-        let Block {
-            id,
-            name,
-            faces,
-            is_plant,
-            ..
-        } = block;
-
-        let lower_name = name.to_lowercase();
-
-        self.blocks_by_name
-            .insert(lower_name.clone(), block.clone());
-        self.blocks_by_id.insert(*id, block.clone());
-        self.name_map.insert(*id, lower_name.clone());
-        self.type_map.insert(lower_name.clone(), *id);
-
-        for side in faces.iter() {
-            if *side == BlockFaces::Diagonal && !is_plant {
-                panic!("Non-plant blocks cannot have diagonal textures: {name}");
-            }
-
-            let side_name = Registry::make_side_name(name, side);
-            self.textures.insert(side_name);
-        }
-    }
-
+    /// Generate a faces map. A faces map is a six-property hash map that stores the keys px, py, pz, nx, ny, nz.
+    /// This generates such a struct from a list of faces, according to the faces priorities.
+    ///
+    /// Example: `["All", "Px"]` would result in:
+    /// ```
+    /// {
+    ///    "Px": "Px",
+    ///    "Py": "All",
+    ///    "Pz": "All",
+    ///    "Nx": "All",
+    ///    "Ny": "All",
+    ///    "Nz": "All",
+    /// }
+    /// ```
     pub fn get_faces_map(faces: &Vec<BlockFaces>) -> HashMap<BlockFaces, BlockFaces> {
         let mut faces_map = HashMap::new();
         let sides = vec![
@@ -309,9 +301,47 @@ impl Registry {
         faces_map
     }
 
+    /// Calculate how many textures should be on each side.
+    fn per_side(&self) -> usize {
+        let mut i = 1.0;
+        let sqrt = (self.textures.len() as f32).sqrt().ceil();
+        while i < sqrt {
+            i = i * 2.0;
+        }
+        i as usize
+    }
+
+    /// Record a block into the registry, adding this block into appropriate maps.
+    fn record_block(&mut self, block: &Block) {
+        let Block {
+            id,
+            name,
+            faces,
+            is_plant,
+            ..
+        } = block;
+
+        let lower_name = name.to_lowercase();
+
+        self.blocks_by_name
+            .insert(lower_name.clone(), block.clone());
+        self.blocks_by_id.insert(*id, block.clone());
+        self.name_map.insert(*id, lower_name.clone());
+        self.type_map.insert(lower_name.clone(), *id);
+
+        for side in faces.iter() {
+            if *side == BlockFaces::Diagonal && !is_plant {
+                panic!("Non-plant blocks cannot have diagonal textures: {name}");
+            }
+
+            let side_name = Registry::make_side_name(name, side);
+            self.textures.insert(side_name);
+        }
+    }
+
     /// Fixing texture bleeding with the
     /// [Half-texel edge correction method](http://drilian.com/2008/11/25/understanding-half-pixel-and-half-texel-offsets/)
-    pub fn fix_texture_bleeding(
+    fn fix_texture_bleeding(
         (start_u, start_v, end_u, end_v): (f32, f32, f32, f32),
     ) -> (f32, f32, f32, f32) {
         let offset = 0.1 / 128 as f32;
@@ -323,9 +353,10 @@ impl Registry {
         )
     }
 
-    pub fn make_side_name(name: &str, side: &BlockFaces) -> String {
+    /// Create a name for the side texture.
+    fn make_side_name(name: &str, side: &BlockFaces) -> String {
         format!(
-            "{}__${}",
+            "{}__{}",
             name.to_lowercase(),
             side.to_string().to_lowercase()
         )
