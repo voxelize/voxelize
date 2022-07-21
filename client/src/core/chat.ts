cd0f4d87e2157538ced61b68e1dfecab300154f9
Ian Huang <21ianh1@students.tas.tw>
2022-07-21 01:38:37 -0700

    feat: commands to chat

--- client/src/core/chat.ts
+++ client/src/core/chat.ts
@@ -2,10 +2,35 @@ import { MessageProtocol, ChatProtocol } from "@voxelize/transport/src/types";
 
 import { NetIntercept } from "./network";
 
+/**
+ * A process that gets run when a command is triggered.
+ */
+export type CommandProcessor = (rest: string) => void;
+
 export class Chat implements NetIntercept {
+  private commands: Map<string, CommandProcessor> = new Map();
+
   public packets: MessageProtocol[] = [];
 
+  constructor(public commandSymbol = "/") {}
+
   send = (chat: ChatProtocol) => {
+    if (chat.body.startsWith(this.commandSymbol)) {
+      const words = chat.body
+        .substring(this.commandSymbol.length)
+        .split(" ")
+        .filter(Boolean);
+      const trigger = words.shift();
+      const rest = words.join(" ");
+
+      const process = this.commands.get(trigger);
+
+      if (process) {
+        process(rest.trim());
+        return;
+      }
+    }
+
     this.packets.push({
       type: "CHAT",
       chat,
@@ -13,6 +38,48 @@ export class Chat implements NetIntercept {
   };
 
   onChat: (chat: ChatProtocol) => void;
+
+  /**
+   * Add a command to the chat system. Commands are case sensitive.
+   *
+   * @param trigger - The text to trigger the command, needs to be one single word without spaces.
+   * @param process - The process run when this command is triggered.
+   */
+  addCommand = (
+    trigger: string,
+    process: CommandProcessor,
+    aliases: string[] = []
+  ) => {
+    if (this.commands.has(trigger)) {
+      throw new Error(`Command trigger already taken: ${trigger}`);
+    }
+
+    if (trigger.split(" ").length > 1) {
+      throw new Error("Command trigger must be one word.");
+    }
+
+    this.commands.set(trigger, process);
+
+    for (const alias of aliases) {
+      if (this.commands.has(alias)) {
+        console.warn(
+          `Command alias for "${trigger}", "${alias}" ignored as already taken.`
+        );
+        continue;
+      }
+
+      this.commands.set(alias, process);
+    }
+  };
+
+  /**
+   * Remove a command from the chat system. Case sensitive.
+   *
+   * @param trigger - The trigger to remove.
+   */
+  removeCommand = (trigger: string) => {
+    return !!this.commands.delete(trigger);
+  };
 
   onMessage = (message: MessageProtocol) => {
     if (message.type !== "CHAT") return;
