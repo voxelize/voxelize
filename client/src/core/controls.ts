fddbf4de9cd76c3ad39427b42a12b190ea130bce
Shaoru Ian Huang <ian1314159@gmail.com>
2022-10-21 14:40:07 -0500

    feat: teleport to top block

--- client/src/core/controls.ts
+++ client/src/core/controls.ts
@@ -693,8 +693,13 @@ export class RigidControls extends EventEmitter {
   };
 
   teleport = (vx: number, vy: number, vz: number) => {
-    const maxHeight = this.world.getMaxHeightByVoxel(vx, vz);
-    this.setPosition(vx + 0.5, maxHeight + this.params.bodyHeight, vz + 0.5);
+    this.setPosition(vx + 0.5, vy + this.params.bodyHeight, vz + 0.5);
+  };
+
+  teleportToTop = () => {
+    const { x, z } = this.getPosition();
+    const maxHeight = this.world.getMaxHeightByWorld(x, z);
+    this.teleport(Math.floor(x), maxHeight, Math.floor(z));
   };
 
   /**
