31b1024e991e50988f7c6efc24890425e83ca79b
Ian Huang (Shaoru) <ian1314159@gmail.com>
2023-06-11 02:33:01 +0800

    feat: nuke client side legacy code

--- packages/core/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "shared-worker:*" {
-  const SharedWorkerFactory: new () => SharedWorker;
-  export default SharedWorkerFactory;
-}
-
-declare module "*.glsl" {
-  const value: string;
-  export default value;
-}
--- packages/core/src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./dom-utils";
-export * from "./block-utils";
-export * from "./chunk-utils";
-export * from "./dom-utils";
-export * from "./light-utils";
-export * from "./math-utils";
