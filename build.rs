d12e54a03ed63b8d522a6cf0b66298235ecf1aa1
Ian Huang (Shaoru) <ian1314159@gmail.com>
2024-04-21 14:45:56 -0700

    chore: update build script

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() {
     let mut prost_build = prost_build::Config::new();
     prost_build
+        .protoc_arg("--experimental_allow_proto3_optional")
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile_protos(&["./messages.proto"], &["."])
         .unwrap();
