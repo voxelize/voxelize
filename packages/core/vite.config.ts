e59cdd4daa193a4d8e35bc21e2592ff54ad73414
remorses <beats.by.morse@gmail.com>
2024-05-14 15:34:49 +0200

    fixed transport, removed unused and broken Transport class, fixed bundled workers

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -6,8 +6,12 @@ import { glslify } from "vite-plugin-glslify";
 
 export default defineConfig({
   plugins: [glslify(), externalizeDeps()],
-
+  define: {
+    __buildVersion__: require("./package.json").version,
+  },
+  base: "./",
   build: {
+    minify: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: path.resolve(__dirname, "src/index.ts"),
--- packages/transport/vite.config.ts
+++ packages/transport/vite.config.ts
@@ -6,10 +6,10 @@ import { glslify } from "vite-plugin-glslify";
 
 export default defineConfig({
   plugins: [glslify(), externalizeDeps()],
-
+  base: "./",
   build: {
+    minify: false,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "index",
       formats: ["es", "cjs"],
